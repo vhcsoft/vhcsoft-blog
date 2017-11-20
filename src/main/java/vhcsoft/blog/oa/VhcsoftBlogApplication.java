e468961356f26d0f519974fc08fde8b26396896b
vhcdev <chu.viet.hung.vn@gmail.com>
2017-11-20 11:23:19 +0100

    init project

---
+++ src/main/java/vhcsoft/blog/oa/VhcsoftBlogApplication.java
@@ -0,0 +1,12 @@
+package vhcsoft.blog.oa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VhcsoftBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VhcsoftBlogApplication.class, args);
+	}
+}
